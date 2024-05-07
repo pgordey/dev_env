f959631e3689fba137291c36f91ca9a7cf30305d
pgordey <gordey1233@gmail.com>
2024-05-07 14:35:49 +0300

    Начальная стадия интернет магазина

---
+++ app1/main/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+
+def index(request):
+    context = {
+        'title': 'Home - Главная',
+        'content': "Магазин мебели HOME"
+        
+        }
+    return render(request, 'main/index.html', context)
+
+def about(request):
+    context = {
+        'title': 'Home - О нас',
+        'content': "О нас",
+        'text_on_page': "Почему этот магазин классный"
+        
+        }
+    return render(request, 'main/about.html', context)
